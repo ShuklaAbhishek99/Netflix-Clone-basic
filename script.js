5d0bfb3b74e077aecc01d1c1936e018a2d6c817c
Abhishek <abhishekshukla821@gmail.com>
2024-03-06 23:38:32 +0530

    style: all styling done for lg devices
    faq dropdown active

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("welcome to netflix")
+console.log("welcome to Netflix");
+
+const headings = document.querySelectorAll(".faq-heading");
+
+headings.forEach((heading) => {
+    heading.addEventListener("click", (e) => {
+        const dropDown = heading.nextElementSibling;
+        const display = dropDown.style.display;
+
+        if (display === "none") {
+            dropDown.style.display = "block";
+        } else {
+            dropDown.style.display = "none";
+        }
+    });
+});
